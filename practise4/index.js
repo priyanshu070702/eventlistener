af271564a554cbb430aa4fb14f7260c26f56234c
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2022-06-19 00:30:03 +0530

    Add files via upload

---
+++ practise4/index.js
@@ -0,0 +1,63 @@
+
+/*document.querySelector("button").addEventListener("click",handleClick);
+
+
+function handleClick(){
+    alert("I got clicked");
+}*/
+
+for(var i=0; i<document.querySelectorAll(".drum").length;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    
+    var buttonInnerHTML = this.innerHTML;
+    
+    makesound(buttonInnerHTML);
+    
+});
+}
+
+/* var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();*/
+
+
+
+    document.addEventListener("keypress", function(event){
+         makesound(event.key); 
+    });
+
+    function makesound(key){
+        switch (key) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+            break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+            break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+            break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+            break;
+            case "j":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+            break;
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+            break;
+            case "l":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+            break;
+        
+            default:
+                break;
+            }
+    }
